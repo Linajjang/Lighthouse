e3bcaaf04ae09c486b240fa1c5f3187069dc39d1
Linajjang <linakim@jejuesl.com>
2016-06-04 21:59:24 -0600

    test commit

---
+++ calculator.rb
@@ -0,0 +1,33 @@
+class Calculator
+    
+  def initialize(a,b)
+        @a = a
+        @b = b
+  end
+    
+  def add
+   puts @a + @b
+  end
+  
+  def subtract
+    puts @a - @b
+  end
+  
+  def multiply
+    puts @a * @b
+   end
+   
+   def divide
+    puts @a / @b
+    end
+end 
+
+addition = Calculator.new(2,4)
+subtraction = Calculator.new(2,4)
+multiplication = Calculator.new(2,4)
+division = Calculator.new(4,2)
+
+addition.add
+subtraction.subtract
+multiplication.multiply
+division.divide
